95545034e0021f61e766e2380f57e72c33e764d0
TarikElha <www.elhantiti.t@gmail.com>
2021-11-23 17:30:00 +0100

    updated methods switchOn and off in car and bycicle class

--- Bicycle.php
+++ Bicycle.php
@@ -2,7 +2,12 @@
 
 require_once 'Vehicle.php';
 
-class Bicycle extends Vehicle
+class Bicycle extends Vehicle implements LightableInterface
 {
-    
+    public function switchOn():bool{
+        return true;
+    }
+    public function switchOff():bool{
+        return false;
+    }
 }
